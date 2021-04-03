de480c345c29e6d6b049d29edc90e90327e645f9
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-04-03 19:43:06 -0300

    add schema.sql

--- src/main/java/br/com/nt/DemoApplication.java
+++ src/main/java/br/com/nt/DemoApplication.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Logger;
+
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 public class DemoApplication {
 
@@ -23,19 +26,31 @@ public class DemoApplication {
 		Properties properties = new Properties();
 		try {
 			properties.load(DemoApplication.class.getClassLoader().getResourceAsStream("application.properties"));
+
+			log.info("Connecting to the database");
+
+			Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+			log.info("Database connection test: " + connection.getCatalog());
+
+			log.info("Loading database schema");
+			Scanner scanner = new Scanner(DemoApplication.class.getClassLoader().getResourceAsStream("schema.sql"));
+			Statement statement = connection.createStatement();
+			while (scanner.hasNext()) {
+				statement.execute(scanner.nextLine());
+
+			}
+			log.info("Create database schema");
+			log.info("Closing database connection");
+			connection.close();
+			AbandonedConnectionCleanupThread.uncheckedShutdown();
 		} catch (IOException e) {
 
 			e.printStackTrace();
-		}
-
-		log.info("Connecting to the database");
-		try {
-			Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
-			log.info("Database connection test: " + connection.getCatalog());
 		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
+
 	}
 
 }
